4a0831e548eb928ec16816f2702205baac9cc790
chilemati <amadichile007@gmail.com>
2025-04-28 05:24:40 +0100

    v2.1a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/logo.png" sizes="32" />
+      <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="">
       <AppRouterCacheProvider>
