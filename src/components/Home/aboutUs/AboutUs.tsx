d8ab3321a17c7aa187f32f0ec66dab9a3abc3ec9
James Favour <68597611+sage-rsc@users.noreply.github.com>
2025-08-07 01:49:36 +0100

    removed derived from crpto market kings

--- src/components/Home/aboutUs/AboutUs.tsx
+++ src/components/Home/aboutUs/AboutUs.tsx
@@ -14,7 +14,7 @@ const AboutUs = () => {
         </Link>
       </div>
       <div className="w-full max-w-4xl mx-auto flex flex-col gap-6 animate-fade-in-up delay-150">
-        <h3 className="text-2xl md:text-3xl font-bold text-gray-900 mt-4">At Blacktinum, we&apos;ve redefined the way you trade cryptocurrencies. Derived from &quot;Crypto Market Kings,&quot; we combine cutting-edge artificial intelligence algorithms with a range of innovative trading tools.</h3>
+        <h3 className="text-2xl md:text-3xl font-bold text-gray-900 mt-4">At Blacktinum, we&apos;ve redefined the way you trade cryptocurrencies. We combine cutting-edge artificial intelligence algorithms with a range of innovative trading tools.</h3>
         <LearnMore />
         <p className="text-base md:text-lg text-gray-800 leading-relaxed">
           At Blacktinum, we unveil the power of exclusive, closely guarded trading strategies that have been honed over several years of meticulous refinement. These unique strategies, crafted with precision and tested rigorously for over six years, have finally been made accessible to the public. This groundbreaking revelation is your gateway to unparalleled success in the world of crypto trading.
